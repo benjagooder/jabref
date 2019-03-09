8467cf7b4aeb8f1af1611616b51c7e95e473c237
Benja Gooder <benjamingooder@gmail.com>
2019-03-09 11:48:46 -0500

    Circle CI error purge

--- src/test/java/org/jabref/logic/util/strings/StringSimilarityTest.java
+++ src/test/java/org/jabref/logic/util/strings/StringSimilarityTest.java
@@ -3,10 +3,9 @@ package org.jabref.logic.util.strings;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringSimilarityTest {
 
