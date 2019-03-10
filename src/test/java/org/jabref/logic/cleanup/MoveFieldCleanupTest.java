7feb17417e27ab84ee1260a5ad60d3d5f9f8b7d9
Benja Gooder <benjamingooder@gmail.com>
2019-03-10 11:15:58 -0400

    Circle CI error purge

--- src/test/java/org/jabref/logic/cleanup/MoveFieldCleanupTest.java
+++ src/test/java/org/jabref/logic/cleanup/MoveFieldCleanupTest.java
@@ -1,12 +1,12 @@
 package org.jabref.logic.cleanup;
 
-import java.util.List;
-
 import org.jabref.model.FieldChange;
 import org.jabref.model.entry.BibEntry;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -17,10 +17,12 @@ public class MoveFieldCleanupTest {
    private String mockDestination = "sent";
    private List<FieldChange> testReturn;
    private MoveFieldCleanup sut;
+
    @BeforeEach
    void setup(){
        sut = new MoveFieldCleanup(mockSource, mockDestination);
    }
+
     @Test
     public void givenNoArgsConstructorBibEntryWhenCleanupThenRuns(){
        testReturn = sut.cleanup(temp);
