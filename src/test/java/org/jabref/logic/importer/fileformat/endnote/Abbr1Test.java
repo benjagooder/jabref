a918e37f4828cfa2d95bd83fb50eda7688ea604d
John Besse <john.a.besse@gmail.com>
2019-04-07 18:20:47 -0400

    Create Abbr1Test.java
    Abbr1 and Style unit tests

---
+++ src/test/java/org/jabref/logic/importer/fileformat/endnote/Abbr1Test.java
@@ -0,0 +1,17 @@
+package org.jabref.logic.importer.fileformat.endnote;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Abbr1Test {
+
+    @Test
+    void givenStyle_whenGetStyle_thenGetStyle() {
+        Abbr1 sut = new Abbr1();
+        Style newStyle = new Style();
+        sut.setStyle(newStyle);
+        Style sutStyle = sut.getStyle();
+        assertEquals(newStyle, sutStyle);
+    }
+}
